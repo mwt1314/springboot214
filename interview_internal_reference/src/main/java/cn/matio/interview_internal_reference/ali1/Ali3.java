f5abedad59224854d4b707dad1592ad489ef0d42
mawt <13437198510@163.com>
2020-01-01 22:50:52 +0800

    Default Changelist

--- interview_internal_reference/src/main/java/cn/matio/interview_internal_reference/ali1/Ali3.java
+++ interview_internal_reference/src/main/java/cn/matio/interview_internal_reference/ali1/Ali3.java
@@ -1,11 +1,63 @@
 package cn.matio.interview_internal_reference.ali1;
 
 /**
- *
  * 01.阿里篇/1.1.3 给定一个二叉搜索树(BST)，找到树中第 K 小的节点.md
- *
+ * https://github.com/0voice/interview_internal_reference/blob/master/01.%E9%98%BF%E9%87%8C%E7%AF%87/1.1.3%20%E7%BB%99%E5%AE%9A%E4%B8%80%E4%B8%AA%E4%BA%8C%E5%8F%89%E6%90%9C%E7%B4%A2%E6%A0%91(BST)%EF%BC%8C%E6%89%BE%E5%88%B0%E6%A0%91%E4%B8%AD%E7%AC%AC%20K%20%E5%B0%8F%E7%9A%84%E8%8A%82%E7%82%B9.md
  */
 public class Ali3 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private class ResultType {
+
+        boolean found;  // 是否找到
+
+        int val;  // 节点数目
+
+        ResultType(boolean found, int val) {
+            this.found = found;
+            this.val = val;
+        }
+    }
+
+    public int kthSmallest(TreeNode root, int k) {
+        return kthSmallestHelper(root, k).val;
+    }
+
+    private ResultType kthSmallestHelper(TreeNode root, int k) {
+        if (root == null) {
+            return new ResultType(false, 0);
+        }
+
+        ResultType left = kthSmallestHelper(root.left, k);
+
+        // 左子树找到，直接返回
+        if (left.found) {
+            return new ResultType(true, left.val);
+        }
+
+        // 左子树的节点数目 = K-1，结果为 root 的值
+        if (k - left.val == 1) {
+            return new ResultType(true, root.val);
+        }
+
+        // 右子树寻找
+        ResultType right = kthSmallestHelper(root.right, k - left.val - 1);
+        if (right.found) {
+            return new ResultType(true, right.val);
+        }
+
+        // 没找到，返回节点总数
+        return new ResultType(false, left.val + 1 + right.val);
+    }
 
     public static void main(String[] args) {
 
